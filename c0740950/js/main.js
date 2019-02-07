9cdff88a249ef51f5ff294fc061c62c30d926256
visualstudio <visualstudio@230-PC20>
2019-02-07 11:42:47 -0500

    //pens

--- c0740950/js/main.js
+++ c0740950/js/main.js
@@ -1,1 +1,22 @@
 ﻿// Your code here!
+ function myfunction(a, b) {
+    
+    return a + b;  //the function return the product of p1 and p2
+}
+
+
+
+function arrays() {
+    var pens;
+    pens = ["red", "orange", "blue", "green", "pink", "purple"];
+    console.log(pens);
+
+}
+
+
+
+
+
+
+
+
